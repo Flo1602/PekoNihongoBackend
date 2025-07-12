36637a9b495ccc07a293e94bdbaff1d5981119e6
Flo1602 <flo16022003@gmail.com>
2025-07-12 10:41:24 +0200

    first commit

---
+++ src/main/java/at/primetshofer/pekoNihongoBackend/PekoNihongoBackendApplication.java
@@ -0,0 +1,13 @@
+package at.primetshofer.pekoNihongoBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PekoNihongoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PekoNihongoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/primetshofer/pekoNihongoBackend/PekoNihongoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.primetshofer.pekoNihongoBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PekoNihongoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
